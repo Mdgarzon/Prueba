3e0f603572918ad52f1477b0f6182b0fd444a383
mdgarzon1323 <mitchel.garzon.mt@usa.edu.co>
2022-12-02 20:31:55 -0500

    first commit

---
+++ js/1996.js
@@ -0,0 +1,21 @@
+let numeroUno, numeroDos;
+let diferencia;
+
+numeroUno= 10;
+numeroDos= 8;
+
+if(numeroUno > numeroDos) {
+    document.write("El numero mayor es:" + numeroUno + "</br>" );
+    diferencia = numeroUno -numeroDos;
+    document.write("la diferencia es:" + diferencia)
+
+    }else{
+        document.write("el numero mayor es:" + numeroDos)
+        diferencia = numeroDos - numeroUno;
+        document.write("la diferencia es:" + diferencia);
+
+    }
+
+
+
+
